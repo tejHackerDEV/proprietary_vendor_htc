d88f7296ab0d3caffe478ea527a30f0bc62aabf2
Shane Passmore <h8rift@gmail.com>
2013-01-06 21:05:15 -0600

    evita: update evita-vendor-blobs.mk

--- evita/evita-vendor-blobs.mk
+++ evita/evita-vendor-blobs.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
 	vendor/htc/evita/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
 	vendor/htc/evita/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
 	vendor/htc/evita/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
+	vendor/htc/evita/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
 	vendor/htc/evita/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
 	vendor/htc/evita/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
 	vendor/htc/evita/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
