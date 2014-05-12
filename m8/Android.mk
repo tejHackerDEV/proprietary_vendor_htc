09de6d33dc0425fd16bab537e7a5892bfd4c214b
invisiblek <dan.pasanen@gmail.com>
2014-05-12 16:26:59 -0500

    m8: commonize libtime_genoff
    Change-Id: Ifc8240101d9c6d33d478191829a1aab3934c51f9

--- m8/Android.mk
+++ m8/Android.mk
@@ -29,6 +29,15 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := htc
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
 endif
 endif
 
