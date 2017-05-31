43afc9a86c429a54dbf54b26ea85011f05c24986
Josue Rivera <prbassplayer@gmail.com>
2017-06-01 01:57:54 +0200

    flounder: Fix licensing
      * Add back original CM licensing as these tree's where CM's originally
      * Kept PureNexus  on flounder-vendor.mk as we did use their updates
      * Added SlimRoms as I've been updating for a while
    
    Change-Id: Ie5b7ae12568a82f8eca4395e1b0b22417d8e0a18
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- flounder/flounder-vendor.mk
+++ flounder/flounder-vendor.mk
@@ -1,4 +1,6 @@
+# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2015 The Pure Nexus Project
+# Copyright (C) 2016-2017 SlimRoms Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
